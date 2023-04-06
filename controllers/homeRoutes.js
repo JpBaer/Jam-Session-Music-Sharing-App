c0dd3f011307cd61697ad91860321d270c9e62d2
Nathan <119708133+nathantr97@users.noreply.github.com>
2023-04-05 17:33:11 -0700

    setting up homeRoutes. homeroutes still need utils and profile.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,2 +1,76 @@
 const router = require('express').Router();
 const { User } = require('../models');
+//need to create utils folder that contains auth.js and link it here
+// projectData will need to be replaced with our model later on
+router.get('/', async (req, res) => {
+    try {
+      const projectData = await Project.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      const projects = projectData.map((project) => project.get({ plain: true }));
+  
+      res.render('homepage', { 
+        projects, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get('/project/:id', async (req, res) => {
+    try {
+      const projectData = await Project.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      const project = projectData.get({ plain: true });
+  
+      res.render('project', {
+        ...project,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  //profile.js  file will be in public folder when done setting up
+  router.get('/profile', withAuth, async (req, res) => {
+    try {
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Project }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  module.exports = router;
